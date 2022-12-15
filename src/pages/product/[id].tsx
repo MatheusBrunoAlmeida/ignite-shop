92e00d7f8f838b699d485d9804e50ff1155b197d
MatheusBrunoAlmeida <manancialsup@hotmail.com>
2022-12-15 02:47:27 -0300

    adicionando fallback do SSG

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -3,18 +3,24 @@ import { stripe } from "../../lib/stripe";
 import Stripe from 'stripe'
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
-interface ProductProps{
+interface ProductProps {
     product: {
-        id: string;
-        name: string;
-        imageUrl: string;
-        price: string;
-        description: string; 
-      }
-}
+      id: string
+      name: string
+      imageUrl: string
+      price: string
+      description: string
+    }
+  }
 
 export default function Product({product}: ProductProps) {
+    const { isFallback } = useRouter()
+
+    if(isFallback){
+        return <p>Loading...</p>
+    }
 
     return (
         <ProductContainer>
@@ -41,34 +47,34 @@ export const getStaticPaths: GetStaticPaths = async () =>{
         paths: [
             { params: {id: 'prod_Mz6tyOnbkfE77V'}}
         ],
-        fallback: false
+        fallback: true
     }
 }
 
 export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
     // @ts-ignore
     const productId = params.id;
-
-    const product = stripe.products.retrieve(productId, {
-        expand: ['default_price'],
-    })
-
-    const price = product.default_price as Stripe.Price
-
+  
+    const product = await stripe.products.retrieve(productId, {
+      expand: ['default_price']
+    });
+  
+    const price = product.default_price as Stripe.Price;
+  
     return {
-        props: {
-            product: {
-                id: product.id,
-                name: product.name,
-                imageUrl: product.images[0],
-                price: new Intl.NumberFormat('pt-BR', {
-                    style: 'currency',
-                    currency: 'BRL'
-                    // @ts-ignore
-                }).format(price.unit_amount / 100),
-                description: product.description
-            }
-        },
-        revalidate: 60 * 60 * 1 // 1 hour
+      props: {
+        product: {
+          id: product.id,
+          name: product.name,
+          imageUrl: product.images[0],
+          price: new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+            // @ts-ignore
+          }).format(price.unit_amount / 100),
+          description: product.description
+        }
+      },
+      revalidate: 60 * 60 * 1 // 1 hours
     }
-}
+  }
