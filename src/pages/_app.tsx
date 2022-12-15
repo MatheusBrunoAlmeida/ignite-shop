cde26ed1f75e7a62e919ae6f5c3b1fb81f914df4
MatheusBrunoAlmeida <manancialsup@hotmail.com>
2022-12-15 00:41:42 -0300

    start the project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
+
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from '../styles/pages/app'
+import Image from 'next/image'
+
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg}  alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
+
+export default function Home() {
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta 1</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta 2</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  )
+}
