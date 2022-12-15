0de85be2cebcb19cd67e8496c26c3d38d7b1d02c
MatheusBrunoAlmeida <manancialsup@hotmail.com>
2022-12-15 02:50:59 -0300

    corrigindo o prefetch dos links para melhorar a performace

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,7 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return (
-          <Link href={`/product/${product.id}`} key={product.id}>
+          <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
             <Product className="keen-slider__slide">
               <Image src={product.imageUrl} width={520} height={480} alt="" />
 
